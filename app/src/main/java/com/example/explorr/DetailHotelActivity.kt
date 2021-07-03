28e7319e5546e0e9ae928579d68e75839ecba29c
Santoni Dyaz <tonisan1080p@gmail.com>
2021-07-04 06:20:49 +0700

    add base fragment and setup bottom navigation

---
+++ app/src/main/java/com/example/explorr/DetailHotelActivity.kt
@@ -0,0 +1,11 @@
+package com.example.explorr
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailHotelActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_hotel)
+    }
+}
--- app/src/main/java/com/example/explorr/MainActivity.kt
+++ app/src/main/java/com/example/explorr/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.explorr
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setBottomNav()
+
     }
+
+    private fun setBottomNav(){
+        val navView = findViewById<BottomNavigationView>(R.id.nav_view)
+        val navController = Navigation.findNavController(this, R.id.bottom_menu_fragment)
+        NavigationUI.setupWithNavController(navView, navController)
+    }
+
 }
